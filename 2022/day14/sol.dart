1e6511368ee71eae2c8a61a8d89b59b37729f5ec
Manuel Pineda <manuelpineda@google.com>
2022-12-16 22:00:44 +0100

    AoC 2022, day 14.

--- 2022/day14/sol.dart
+++ 2022/day14/sol.dart
@@ -3,42 +3,11 @@ import 'dart:io';
 import 'dart:convert';
 import 'dart:collection';
 import 'dart:math';
-
-int sign(int x) {
-  if (x > 0) return 1;
-  if (x < 0) return -1;
-  return 0;
-}
+import '../pos.dart';
 
 Future<List<String>> readLines(String filename) async {
   String contents = await File(filename).readAsString();
   return contents.split('\n');
-}
-
-class Pos {
-  int x, y;
-  Pos(this.x, this.y);
-  
-  String toString() {
-    return "{$x, $y}";
-  }
-  
-  bool operator ==(Object o) {
-    Pos other = o as Pos;
-    return x == o.x && y == o.y;
-  }
-
-  int get hashCode => Object.hash(x, y);
-
-  Pos nextTowards(Pos t) {
-    int dx = sign(t.x - this.x);
-    int dy = sign(t.y - this.y);
-    return Pos(this.x + dx, this.y + dy);
-  }
-
-  Pos nextDown() => Pos(x + 1, y);
-  Pos nextDownLeft() => Pos(x + 1, y - 1);
-  Pos nextDownRight() => Pos(x + 1, y + 1);
 }
 
 void main(List<String> args) async {
