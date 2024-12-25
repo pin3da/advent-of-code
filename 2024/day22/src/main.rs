f871abf2d4af22f1a18978dc11dbc97873403b97
Manuel Pineda <manuelpineda@google.com>
2024-12-25 15:50:56 +0100

    Improve day 22, just check the sequences that possible.

--- 2024/day22/src/main.rs
+++ 2024/day22/src/main.rs
@@ -3,28 +3,6 @@ use std::collections::{HashMap, HashSet};
 
 const MOD: i64 = 16777216;
 
-fn gen_sequence() -> Vec<Vec<i64>> {
-    fn int_to_vec(n: i64) -> Vec<i64> {
-        let mut digits = vec![];
-        let mut n = n;
-        while n > 0 {
-            digits.push((n % 10) as i64);
-            n /= 10;
-        }
-        digits.reverse();
-        while digits.len() < 5 {
-            digits.insert(0, 0);
-        }
-        digits
-    }
-
-    let mut sequence = vec![];
-    for i in 0..=99999 {
-        sequence.push(int_to_vec(i));
-    }
-    sequence
-}
-
 fn main() {
     let input = read_input("./src/example.txt")
         .lines()
@@ -39,36 +17,22 @@ fn main() {
 
     let start_time = std::time::Instant::now();
 
-    let mut best_per_seed: Vec<HashMap<Vec<i64>, i64>> = vec![];
+    let mut total_by_seq: HashMap<Vec<i64>, i64> = HashMap::new();
     for seed in &input {
-        let mut value_by_diff = HashMap::new();
         let secret = generate_secrets(*seed, 2000).iter().map(|v| v % 10).collect::<Vec<i64>>();
         let windows = secret.windows(5);
+        let mut seen_seq = HashSet::new();
         for w in windows {
             let diff = w.windows(2).map(|d| d[1] - d[0]).collect::<Vec<i64>>();
-            if value_by_diff.contains_key(&diff) {
+            if seen_seq.contains(&diff) {
                 continue;
             }
-            value_by_diff.insert(diff, w[4]);
+            seen_seq.insert(diff.clone());
+            total_by_seq.entry(diff).and_modify(|v| *v += w[4]).or_insert(w[4]);
         }
-        best_per_seed.push(value_by_diff);
     }
 
-    let sequence: Vec<Vec<i64>> = gen_sequence()
-        .iter()
-        .map(|v| v.windows(2).map(|w| w[1] - w[0]).collect::<Vec<i64>>())
-        .collect::<HashSet<_>>()
-        .into_iter()
-        .collect();
-
-    let part2 = sequence.iter()
-        .map(|s| {
-            best_per_seed.iter()
-                .filter_map(|best| best.get(s))
-                .sum::<i64>()
-        })
-        .max()
-        .unwrap_or(0);
+    let part2 = total_by_seq.values().max().unwrap_or(&0);
 
     // 1608 too low -> Introduced a bug in the generation of the sequence.
     println!("Part 2: {}", part2);
