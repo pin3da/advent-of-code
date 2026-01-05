0d6c7617b54dedabad351da7126cdb4927f0e16e
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2026-01-05 20:47:52 +0100

    Clean up day 6.

--- 2025/day06.rb
+++ 2025/day06.rb
@@ -1,46 +1,29 @@
 lines = $stdin.each_line.reject { |l| l.strip.empty? }
 
 seps = (0...lines.first.length).select { |i| lines.all? { |l| l[i] == ' ' } }
-seps = [0] + seps.map{|i| i + 1} + [lines.first.length]
+seps = [0] + seps.map{ |i| i + 1 } + [lines.first.length]
 
 grid = lines.map do |line|
   seps.each_cons(2).map { |s, e| line[s...(e-1)] }
 end
 
-grid.last.map!(&:strip)
+ops = grid.pop.map(&:strip)
 
-last = grid.length - 1
+part1 = grid.first.each_index.sum { |col|
+  grid.map {|row| row[col].to_i }.inject(ops[col])
+}
 
-total = 0
-grid.first.each_index do |col|
-  vals = (0...last).map do |row|
-    grid[row][col].to_i
-  end
-  op = grid[last][col]
-  total += vals.inject(op)
-end
+puts "Part 1: #{part1}"
 
-puts "Part 1: #{total}"
-
-total = 0
-
-grid.first.each_index do |col|
+part2 = grid.first.each_index.sum { |col|
   block_size = grid.first[col].length
 
-  vals = (0..block_size-1).map do |id_in_block|
-    num = 0
-    (0..last-1).each do |row|
-      digit = grid[row][col][id_in_block]
-      next if digit == " "
-      num *= 10
-      num += digit.to_i
-    end
-    num
+  vals = block_size.times.map do |id_in_block|
+    grid.map {|row| row[col][id_in_block] }.join.tr(' ', '').to_i
   end
-  op = grid[last][col]
-  total += vals.inject(op)
-end
+  vals.inject(ops[col])
+}
 
-puts "Part 2: #{total}"
+puts "Part 2: #{part2}"
 
 
